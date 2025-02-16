85cdcca09255a4586ce4b8837d85cce247d13a1e
Jashanpreet Singh <jashanpreetsingh1096@gmail.com>
2025-02-16 16:55:29 +0530

    sockets fixed

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin : ["http://localhost:5173"],
+        origin : ["http://3.82.158.190:8000"],
         methods: ["GET", "POST"]
     }
 });
